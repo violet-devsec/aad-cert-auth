ea14d5e13bc94a629cae66cde1910f2e00b0647d
violet-devsec <titto.tvm@hotmail.com>
2024-03-18 16:49:43 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from azure.identity import CertificateCredential
+from azure.keyvault.secrets import SecretClient
+
+load_dotenv()
+
+TenantId = os.environ["AZURE_TENANT_ID"]
+ClientId = os.environ["AZURE_CLIENT_ID"]
+KVUri = os.environ["KEY_VAULT_URI"]
+CertPath = "./kv-prv.pfx"
+
+print(TenantId)
+
+credential = CertificateCredential(tenant_id=TenantId, client_id=ClientId, certificate_path=CertPath, password="")
+
+kvClient = SecretClient(vault_url=KVUri, credential=credential)
+
+secret = kvClient.get_secret(os.environ["SECRET_NAME"]).value
+
+print(secret)
